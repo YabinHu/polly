f704470146fa7873f3764abd62a689c32e4a6fdf
Yabin Hu <yabin.hwu@gmail.com>
2014-07-09 14:29:36 +0200

    Update may_write and tagged_may_write calculation
    In ppcg, may_write means all the writes (polly's may_writes plus
    must_writes).

--- lib/CodeGen/GPGPU/ppcg.cpp
+++ lib/CodeGen/GPGPU/ppcg.cpp
@@ -1139,6 +1139,12 @@ ppcg_scop_from_polly_scop(Scop *scop, struct ppcg_options *options)
 	ps->tagged_must_writes = pet_scop_collect_tagged_must_writes(ps);
 	ps->must_writes = pet_scop_collect_must_writes(ps);
 	ps->tagged_must_kills = pet_scop_collect_tagged_must_kills(ps);
+	ps->may_writes =
+            isl_union_map_union(ps->may_writes,
+                                isl_union_map_copy(ps->must_writes));
+	ps->tagged_may_writes =
+            isl_union_map_union(ps->tagged_may_writes,
+                                isl_union_map_copy(ps->tagged_must_writes));
 	ps->schedule = collectSchedule(scop);
 	// ps->n_type = scop->n_type;
 	// ps->types = scop->types;
