0d3e9ef498972471e7bb970b9ca565f992def936
Yabin Hu <yabin.hwu@gmail.com>
2014-07-09 09:29:13 +0200

    Add struct gen_ext
    This struct will be used to transmit all the output of the ppcg generated
    isl* structures.
     - context: represents the constraint of the gpu_prog.
     - guard: guard of the host code.
     - tree: host code ast_node.
     - prog: gpu_prog struct.

--- lib/CodeGen/GPGPU/gpu.h
+++ lib/CodeGen/GPGPU/gpu.h
@@ -7,6 +7,13 @@
 #include "ppcg.h"
 #include "ppcg_options.h"
 
+struct gen_ext {
+  isl_set *context;
+  isl_set *guard;
+  isl_ast_node *tree;
+  struct gpu_prog *prog;
+};
+
 /* Represents an outer array possibly accessed by a gpu_prog.
  * If this outer array contains structures, then the references are not
  * collected and the reference groups are not computed.
