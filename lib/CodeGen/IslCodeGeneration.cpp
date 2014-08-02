d558e7e290e146e4a8992263541be97a6c00100d
Yabin Hu <yabin.hwu@gmail.com>
2014-08-03 00:20:43 +0200

    Update IDToValue with GPU ids stored in kernel node

--- lib/CodeGen/IslCodeGeneration.cpp
+++ lib/CodeGen/IslCodeGeneration.cpp
@@ -857,8 +857,22 @@ void IslNodeBuilder::createForGPGPU(__isl_take isl_ast_node *Node,
   BasicBlock::iterator AfterLoop = Builder.GetInsertPoint();
   Builder.SetInsertPoint(KernelBody);
 
-  create(isl_ast_node_copy(Kernel->tree));
   Function *FN = Builder.GetInsertBlock()->getParent();
+
+  // We should fill the IDToValue before this create(node) call.
+  for (int i = 0; i < Kernel->n_gpuid; ++i) {
+    isl_id *GPUId = isl_id_copy(Kernel->gpuid[i]);
+    const char *Name = isl_id_get_name(GPUId);
+    Value *IDValue = PTXGen->getValueOfGPUID(Name);
+    IDToValue[GPUId] = IDValue;
+    isl_id_free(GPUId);
+  }
+
+  create(isl_ast_node_copy(Kernel->tree));
+
+  // Erase the id to value mapping for gpu ids.
+  for (int i = 0; i < Kernel->n_gpuid; ++i)
+    IDToValue.erase(Kernel->gpuid[i]);
 
   // Set back the insert point to host end code.
   Builder.SetInsertPoint(AfterLoop);
